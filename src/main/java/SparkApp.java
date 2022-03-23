56be5fc6d1f31ec648fd07a9f355c887bdc637d5
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-03-23 14:05:08 -0500

    Https protocol

--- src/main/java/SparkApp.java
+++ src/main/java/SparkApp.java
@@ -2,9 +2,18 @@ import static spark.Spark.*;
 
 public class SparkApp {
     public static void main(String[] args) {
-        port(5000);
+        port(getPort());
+
+        secure("keystores/ecikeystore.p12", "123456", null, null);
 
         get("/hello", (req, res) -> "Hello Service");
     }
-    
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000;
+    }
+
 }
